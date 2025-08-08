73c38681e66ac806a5237d463a59da08f37d5da4
kidave <karandave1610@gmail.com>
2025-08-08 23:50:22 +0530

    robot debug

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: 'https://walkingproject.vercel.app', // change to .org if needed
-  generateRobotsTxt: true,
-  sitemapSize: 5000, // optional, for big sites
+  siteUrl: 'https://walkingproject.vercel.app',
+  generateRobotsTxt: false,
+  sitemapSize: 5000, 
   changefreq: 'weekly',
   priority: 0.7,
   exclude: [
-    '/admin*',    // excludes /admin and all subpaths like /admin/settings
-    '/auth*',     // excludes /auth and subpaths
-    '/auth/callback' // optional explicit exclude
+    '/admin*',  
+    '/auth*',     
+    '/auth/callback' 
   ],
 };
