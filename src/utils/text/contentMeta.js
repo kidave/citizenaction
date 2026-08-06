fb4d059c9b6417fae0a0f92aa00646ff1bf77727
kidave <karandave1610@gmail.com>
2026-08-06 08:38:13 +0530

    attachment debug

--- src/utils/text/contentMeta.js
+++ src/utils/text/contentMeta.js
@@ -1,13 +1,20 @@
 export function extractContentMeta(text) {
   if (!text) {
-    return { links: [], hashtags: [] };
+    return {
+      extracted_links: [],
+      hashtags: [],
+    };
   }
 
   const urlRegex = /(https?:\/\/[^\s]+)/g;
   const hashtagRegex = /#(\w+)/g;
 
-  const links = text.match(urlRegex) || [];
+  const extracted_links = text.match(urlRegex) || [];
+
   const hashtags = [...text.matchAll(hashtagRegex)].map((m) => m[1]);
 
-  return { links, hashtags };
+  return {
+    extracted_links,
+    hashtags,
+  };
 }
