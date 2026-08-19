2b485ed5098b649e64db7875be4542a3094cc496
kidave <karandave1610@gmail.com>
2026-08-20 01:52:20 +0530

    user post gap removed

--- src/components/skeletons/UserPostsSkeleton.jsx
+++ src/components/skeletons/UserPostsSkeleton.jsx
@@ -4,7 +4,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function UserPostsSkeleton() {
   return (
-    <div className="grid grid-cols-3 gap-1">
+    <div className="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5">
       {Array.from({ length: 9 }).map((_, index) => (
         <Skeleton key={index} className="aspect-square rounded-none" />
       ))}
