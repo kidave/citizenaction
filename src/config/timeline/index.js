7abf1e0414497c1f03439d994aec15d1b24d57f3
Karan Dave <84247013+kidave@users.noreply.github.com>
2026-08-22 15:25:59 +0530

    fix(timeline): restore shared timeline config

---
+++ src/config/timeline/index.js
@@ -0,0 +1,4 @@
+export { TIMELINE_NATURAL_TEXT } from "./naturalText";
+export { GOVERNANCE_LANGUAGE, GOVERNANCE_RELATIONSHIP_LANGUAGE } from "./governanceLanguage";
+export { TIMELINE_COLORS, getTimelineColor, getTimelineColorForMonth } from "./colors";
+export { TIMELINE_FALLBACK_IMAGES } from "./fallbackImages";
