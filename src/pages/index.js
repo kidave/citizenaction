f422800416f16bcedc60be7066a22e08dfd90300
Karan Dave <84247013+kidave@users.noreply.github.com>
2026-09-13 21:15:58 +0530

    refactor(layout): let home declare right sidebar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,8 @@
 import Feed from "@/components/feed/Feed";
+import AppShell from "@/components/layout/AppShell";
 
 export default function Home() {
   return <Feed />;
 }
+
+Home.getLayout = (page) => <AppShell showRightSidebar>{page}</AppShell>;
