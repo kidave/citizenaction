f70e7461758580a090d6d34a2d7d75078dfba199
Karan Dave <84247013+kidave@users.noreply.github.com>
2026-09-16 00:26:07 +0530

    refactor: remove local floating menu mount

--- src/components/layout/AppShell.js
+++ src/components/layout/AppShell.js
@@ -5,7 +5,6 @@ import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
 import LeftSidebar from "./LeftSidebar";
 import RightSidebar from "./RightSidebar";
 import CenterColumn from "./CenterColumn";
-import FloatingMenu from "./FloatingMenu";
 
 export default function AppShell({ children, showRightSidebar = false }) {
   return (
@@ -16,8 +15,6 @@ export default function AppShell({ children, showRightSidebar = false }) {
         <SidebarInset className="min-w-0 flex-1">
           <CenterColumn>{children}</CenterColumn>
         </SidebarInset>
-
-        <FloatingMenu />
       </SidebarProvider>
 
       {showRightSidebar && (
