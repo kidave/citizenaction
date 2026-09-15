0dba36382eaccc15b514bd932996346c21653a1e
Karan Dave <84247013+kidave@users.noreply.github.com>
2026-09-16 00:26:02 +0530

    feat: show floating menu globally on mobile

--- src/components/layout/FloatingMenu.js
+++ src/components/layout/FloatingMenu.js
@@ -45,7 +45,7 @@ export default function FloatingMenu() {
   ];
 
   return (
-    <div className="fixed bottom-4 right-4 z-50 flex flex-col items-end">
+    <div className="fixed bottom-4 right-4 z-50 flex flex-col items-end md:hidden">
       <AnimatePresence>
         {open && (
           <motion.div
