0516d3fe3807a30b6a7fe884faed7f237fb92b03
Karan Dave <84247013+kidave@users.noreply.github.com>
2026-09-16 09:25:22 +0530

    refactor: remove temporary post type config

--- src/components/feed/PostTypeConfig.js
+++
@@ -1,9 +0,0 @@
-export const POST_DEFAULT_CONFIG = {
-  displayMode: "simple",
-  lifecycle: false,
-  showAddress: true,
-  showTime: true,
-  showJoin: false,
-  showCountdown: false,
-  statuses: {},
-};
